414a78d32d76f96b76994305c8f7c07a52ba7b96
Amos Wenger <amoswenger@gmail.com>
2022-07-12 14:49:46 +0200

    Full repro

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,34 @@ struct SampleError;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-struct SampleLayer;
+struct OuterLayer;
 
-impl<S> Layer<S> for SampleLayer
-where
-    S: Service<SampleRequest>,
-{
-    type Service = SampleService<S>;
+struct BorrowedRequest<'a> {
+    #[allow(dead_code)]
+    req: &'a mut SampleRequest,
+}
+
+impl<S> Layer<S> for OuterLayer {
+    type Service = OuterService<S>;
 
     fn layer(&self, inner: S) -> Self::Service {
-        SampleService { inner }
+        OuterService { inner }
     }
 }
 
 #[derive(Clone)]
-struct SampleService<S> {
+struct OuterService<S> {
     inner: S,
 }
 
-impl<S> Service<SampleRequest> for SampleService<S>
+impl<S> Service<SampleRequest> for OuterService<S>
 where
-    S: Service<SampleRequest, Response = SampleResponse, Error = SampleError>
+    for<'b> S: Service<BorrowedRequest<'b>, Response = SampleResponse, Error = SampleError>
         + Clone
         + Send
+        + Sync
         + 'static,
+    for<'b> <S as Service<BorrowedRequest<'b>>>::Future: Send + 'b,
 {
     type Response = SampleResponse;
     type Error = SampleError;
@@ -47,7 +51,56 @@ where
         self.inner.poll_ready(cx)
     }
 
-    fn call(&mut self, req: SampleRequest) -> Self::Future {
+    fn call(&mut self, mut req: SampleRequest) -> Self::Future {
+        let mut inner = self.inner.clone();
+        std::mem::swap(&mut self.inner, &mut inner);
+
+        async move {
+            let breq = BorrowedRequest { req: &mut req };
+
+            let res = inner.call(breq).await?;
+            Ok(res)
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct MiddleLayer;
+
+impl<S> Layer<S> for MiddleLayer {
+    type Service = MiddleService<S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        MiddleService { inner }
+    }
+}
+
+#[derive(Clone)]
+struct MiddleService<S> {
+    inner: S,
+}
+
+impl<'a, S> Service<BorrowedRequest<'a>> for MiddleService<S>
+where
+    for<'b> S: Service<BorrowedRequest<'b>, Response = SampleResponse, Error = SampleError>
+        + Clone
+        + Send
+        + 'static,
+    for<'b> <S as Service<BorrowedRequest<'b>>>::Future: Send + 'b,
+{
+    type Response = SampleResponse;
+    type Error = SampleError;
+    type Future = impl Future<Output = Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: BorrowedRequest<'a>) -> Self::Future {
         let mut inner = self.inner.clone();
         std::mem::swap(&mut self.inner, &mut inner);
 
@@ -63,7 +116,7 @@ where
 #[derive(Clone)]
 struct InnerService;
 
-impl Service<SampleRequest> for InnerService {
+impl<'a> Service<BorrowedRequest<'a>> for InnerService {
     type Response = SampleResponse;
     type Error = SampleError;
     type Future = impl Future<Output = Result<Self::Response, Self::Error>>;
@@ -75,7 +128,7 @@ impl Service<SampleRequest> for InnerService {
         Ok(()).into()
     }
 
-    fn call(&mut self, req: SampleRequest) -> Self::Future {
+    fn call(&mut self, req: BorrowedRequest<'a>) -> Self::Future {
         async move {
             let _ = req;
             Ok(SampleResponse)
@@ -86,16 +139,20 @@ impl Service<SampleRequest> for InnerService {
 ////////////////////////////////////////////////////////////////////////////////
 
 fn make_http_service() -> BoxCloneService<SampleRequest, SampleResponse, SampleError> {
+    // 👋 uncomment / add more `.layer` lines here to witness compile times
+    // going bonkers.
+
     ServiceBuilder::new()
-        .layer(SampleLayer)
-        .layer(SampleLayer)
-        .layer(SampleLayer)
-        .layer(SampleLayer)
-        .layer(SampleLayer)
-        .layer(SampleLayer)
-        .layer(SampleLayer)
-        .layer(SampleLayer)
-        .layer(SampleLayer)
+        .layer(OuterLayer)
+        .layer(MiddleLayer)
+        .layer(MiddleLayer)
+        .layer(MiddleLayer)
+        .layer(MiddleLayer)
+        .layer(MiddleLayer)
+        // .layer(MiddleLayer)
+        // .layer(MiddleLayer)
+        // .layer(MiddleLayer)
+        // .layer(MiddleLayer)
         .service(InnerService)
         .boxed_clone()
 }
